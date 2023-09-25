110c29b7c291d9913505a0b67bc9ba9265724219
JCLindo10 <JCLindo10@gmail.com>
2023-09-25 14:45:54 -0400

    implemented __str__

--- other.py
+++ other.py
@@ -11,3 +11,10 @@ class Other:
     def __init__(self, base, modifier):
         self.base = base
         self.modifier = modifier
+
+    def __str__(self) -> str:
+        if self.modifier.lower()[0] in 'aeiou':
+            return f'an {self.modifier} {self.base}'
+        else:
+            return f'a {self.modifier} {self.base}'
+            

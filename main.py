0dc481051db288d458cb1e0413256ca80a177055
JCLindo10 <JCLindo10@gmail.com>
2023-09-25 14:48:49 -0400

    splits input on a space

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def main():
     while(True):
 
         #TODO: Make this take in the other and the modifier, splitting on a space
-        input_other = input("What have you encountered?\n")
-        input_modifier = input("what is its modifier?\n")
+        
+        input_modifier, input_other  = input("What have you encountered?\n").split()
+        # input_modifier = input("what is its modifier?\n")
 
         encountered_other = other.Other(input_other, input_modifier)
 
